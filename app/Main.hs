e1ab6819f5e0ae107a8a279ed1640ba106e30d8a
h-alice <admin@halice.art>
2025-05-17 12:13:00 +0800

    Fix some import

--- app/Main.hs
+++ app/Main.hs
@@ -6,8 +6,9 @@ import Network.Wreq
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as TE
 import qualified Data.ByteString.Lazy as BL
-import Control.Monad (void, when)
-import Control.Lens
+import Control.Monad (void, when, (>=>))
+import Control.Lens ((^.))
+import Data.Functor ((<&>))
 import qualified Data.Text.Lazy.Encoding as TLE
 import qualified Data.Text.Lazy as TL
 
@@ -24,7 +25,7 @@ import qualified Data.Text.Encoding as TE
 import Data.ByteString.Builder (toLazyByteString)
 import Data.Aeson (Value(Object))
 
-import Control.Monad
+
 
 ytInfoPrefix :: T.Text
 ytInfoPrefix = "var ytInitialData = " :: T.Text
@@ -38,9 +39,11 @@ youtubeUrlBase = "https://www.youtube.com" :: String
 main :: IO ()
 main = do
     content <- get urlTest
-    let body = content ^. responseBody
-    let doc = parseHtml $ TL.unpack $ TLE.decodeUtf8 body
-    let z = runX (doc >>> css "script" >>> Text.XML.HXT.Core.deep getText) >>= pure . map T.pack >>= pure . filter (T.isPrefixOf ytInfoPrefix)
+
+
+    -- Doc from response body
+    let doc = parseHtml $ TL.unpack $ TLE.decodeUtf8 $ content ^. responseBody
+    let z = (runX (doc >>> css "script" >>> Text.XML.HXT.Core.deep getText) <&> map T.pack) <&> filter (T.isPrefixOf ytInfoPrefix)
     a <- z
     case a of
         [] -> putStrLn "No data found"
